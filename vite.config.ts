dfa1bfd0d7a4a34fbd3cf9cd284287a3e1ca656a
phamtuan19hd <phamtuan19hd@gmail.com>
2023-11-26 15:00:09 +0700

    feat: first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { checker } from "vite-plugin-checker";
+import dynamicImport from "vite-plugin-dynamic-import";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [checker({ typescript: false }), react(), dynamicImport()],
+    server: {
+      port: 3000,
+    },
+    resolve: {
+      alias: {
+      //   "@App": path.resolve(__dirname, "./src/@App"),
+      //   "@Core": path.resolve(__dirname, "./src/@Core"),
+      },
+    },
+  };
+});
