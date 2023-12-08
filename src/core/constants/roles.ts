50c400c13e12d9db1a08d5ea525201e5f55f849e
phamtuan19hd <phamtuan19hd@gmail.com>
2023-12-08 14:35:23 +0700

    feat: config role

---
+++ src/core/constants/roles.ts
@@ -0,0 +1,12 @@
+import * as yup from 'yup';
+
+const ROLE = {
+   USER: 'USER',
+   ADMIN: 'ADMIN',
+};
+
+export const roleSchema = yup.string().oneOf(Object.values(ROLE)).required();
+
+export type RolePropsType = yup.InferType<typeof roleSchema>;
+
+export default ROLE;
---
+++ src/core/hooks/useTheme.tsx
@@ -0,0 +1,5 @@
+const useTheme = () => {
+   return <div></div>;
+};
+
+export default useTheme;
