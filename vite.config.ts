d90f5a46261c3eb46999509dcec2d44527ec8dec
phamtuan19hd <phamtuan19hd@gmail.com>
2023-12-09 15:56:38 +0700

    feat: create common button

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import svgr from 'vite-plugin-svgr';
 import dynamicImport from 'vite-plugin-dynamic-import';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command, mode }) => {
    const env = loadEnv(mode, process.cwd(), '');
 
    return {
