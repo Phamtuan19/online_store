c20a550be372e65be98b1a31945ae40da0f622b2
phamtuan19hd <phamtuan19hd@gmail.com>
2023-12-09 16:13:31 +0700

    feat: delete tailwind

--- src/page/common/Home/index.tsx
+++ src/page/common/Home/index.tsx
@@ -1,11 +1,7 @@
-import Button from '@Core/components/component-ui/Button';
-
 const Home = () => {
    return (
       <div className="px-10 py-10">
-         <Button className="" variant="text" color="primary" endIcon="react">
-            hello
-         </Button>
+         <button className="">hello</button>
       </div>
    );
 };
